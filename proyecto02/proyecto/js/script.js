c1210eac4b98563f8480e11b0dfdd2a026b37d3b
anvefran <anvefran@espol.edu.ec>
2022-11-25 20:26:08 -0500

    Correcciones y validaciones en imagenes

--- proyecto02/proyecto/js/script.js
+++ proyecto02/proyecto/js/script.js
@@ -1,25 +1,28 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+    //showAll();
     cargarDatos();
     mostrarInfo();
 });
 
-let cargarDatos = () => {
-    console.log('DOM cargado y analizado');
-    let url = 'https://pokeapi.co/api/v2/pokemon-species?limit=905'
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            let pokemon = data["results"]
-            let select = document.getElementsByClassName('form-select')[0]          
 
-            pokemon.forEach(function (element) {
-                let option = document.createElement("option")
-                option.innerHTML = element.name
-                select.appendChild(option)
-              });
-            
-        })
-        .catch(console.error);
+
+let cargarDatos = () => {
+  console.log('DOM cargado y analizado');
+  let url = 'https://pokeapi.co/api/v2/pokemon-species?limit=905'
+  fetch(url)
+      .then(response => response.json())
+      .then(data => {
+          let pokemon = data["results"]
+          let select = document.getElementsByClassName('form-select')[0]          
+
+          pokemon.forEach(function (element) {
+              let option = document.createElement("option")
+              option.innerHTML = element.name
+              select.appendChild(option)
+            });
+          
+      })
+      .catch(console.error);
 
 }
 
@@ -39,8 +42,10 @@ let mostrarInfo = () => {
           info.setAttribute("id", "info")
           info.setAttribute("class", "card-body")
           document.getElementById("infoPokemon").appendChild(info)
-
-          document.getElementById("info").innerHTML = `<div class="general"><span>Base experience: ${baseExp}</span></div>`
+          if (baseExp != null){
+            document.getElementById("info").innerHTML = `<div class="general"><span>Base experience: ${baseExp}</span></div>`
+          }
+          
           let height = data["height"]
           document.getElementById("info").innerHTML += `<div class="general"><span>Height: ${height}</span></div>`
           let weight = data["weight"]
@@ -63,11 +68,14 @@ let mostrarInfo = () => {
             `
             document.getElementById("tipos").innerHTML += plantilla
           }
-          let fotopok = document.createElement("img")
-          fotopok.setAttribute("src", `${fotoUrl}`)
-          fotopok.setAttribute("alt", `${valor}`)
-          fotopok.setAttribute("class", `animate__animated animate__backInRight card-img-top`)
-          document.getElementById("infoPokemon").appendChild(fotopok)
+          if (fotoUrl != null){
+            let fotopok = document.createElement("img")
+            fotopok.setAttribute("src", `${fotoUrl}`)
+            fotopok.setAttribute("alt", `${valor}`)
+            fotopok.setAttribute("class", `animate__animated animate__backInRight card-img-top`)
+            document.getElementById("infoPokemon").appendChild(fotopok)
+          }
+          
           document.getElementById("info").innerHTML += `<div class="general"><h6 class="mb-0 text-center">Abilities</h6></div>`
           let abilitiesDiv = document.createElement("div")
           abilitiesDiv.setAttribute("id", "abilities")
