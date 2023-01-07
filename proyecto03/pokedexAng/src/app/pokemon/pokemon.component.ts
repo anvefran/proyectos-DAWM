e3a7a4ec2333d11d091d9287343c19da23cb9cb7
anvefran <anvefran@espol.edu.ec>
2023-01-07 10:53:57 -0500

    Second view DONEEEE

--- proyecto03/pokedexAng/src/app/pokemon/pokemon.component.ts
+++ proyecto03/pokedexAng/src/app/pokemon/pokemon.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RecursosService } from 'src/app/servicios/recursos.service';
 import { PokemonI, Abilities, Stats, Types} from 'src/app/interfaz/pokemon-i';
 import { ActivatedRoute } from '@angular/router';
-
+import Chart from 'chart.js/auto';
 @Component({
   selector: 'app-pokemon',
   templateUrl: './pokemon.component.html',
@@ -10,7 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PokemonComponent {
   pokemon! : PokemonI; //= {} as PokemonI;
-
+  public chart: any;
   constructor(private route: ActivatedRoute, private recursosService: RecursosService) {
   }
   ngOnInit(){
@@ -20,6 +20,26 @@ export class PokemonComponent {
     });
     this.recursosService.getPokemonInfo(nombre).subscribe(respuesta => {
       this.pokemon = respuesta as PokemonI;
+      let arrNum = []
+    let arrNom = []
+    for(let i of this.pokemon.stats){
+      arrNum.push(i.base_stat)
+      arrNom.push(i.stat.name)
+    }
+    this.chart = new Chart("MyChart", {
+      type: 'radar', //this denotes tha type of chart
+
+      data: {
+        labels: arrNom,
+        datasets: [{
+          label: "Statistics",
+          data: arrNum,
+          fill: true,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgb(255, 99, 132)',
+        }]
+      },
+    });
     });
   }
 }
